b974bfafb78a815c307e3a543c1e5d0756962861
Lee-Zijian <470159768@qq.com>
2022-06-20 17:46:20 +0800

    modify some output info

--- server/httpServer.py
+++ server/httpServer.py
@@ -5,6 +5,15 @@ try:
     from waitress import serve
     import os, socket, os.path, argparse, requests
     from banner import *
+    import ssl
+
+    from pyngrok import ngrok, conf, installer
+    pyngrok_config = conf.get_default()
+    if not os.path.exists(pyngrok_config.ngrok_path):
+        myssl = ssl.create_default_context()
+        myssl.check_hostname=False
+        myssl.verify_mode=ssl.CERT_NONE
+        installer.install_ngrok(pyngrok_config.ngrok_path, context=myssl)
 
     os.system("cls" if os.name == "nt" else "clear")
     PrintBanner()
